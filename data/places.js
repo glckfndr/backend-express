ad416be200ef72034c153cc3164c4c829c7b8b99
glckfndr <obulan65@gmail.com>
2025-11-02 09:24:17 +0200

    data: add dummy places

---
+++ data/places.js
@@ -0,0 +1,28 @@
+export const places = [
+  {
+    id: "p1",
+    title: "Empire State Building",
+    description: "One of the most famous skyscrapers in the world",
+    location: { lat: 40.7484405, lng: -73.9878584 },
+    address: "20 W 34th St, New York, NY 10001, United States",
+    creator: "u1",
+  },
+  {
+    id: "p2",
+    title: "Eiffel Tower",
+    description:
+      "A wrought-iron lattice tower on the Champ de Mars in Paris, France.",
+    location: { lat: 48.8588443, lng: 2.2943506 },
+    address: "Champ de Mars, 5 Avenue Anatole France, 75007 Paris, France",
+    creator: "u2",
+  },
+  {
+    id: "p3",
+    title: "Colosseum",
+    description:
+      "An ancient amphitheater located in the center of Rome, Italy.",
+    location: { lat: 41.8902102, lng: 12.492231 },
+    address: "Piazza del Colosseo, 1, 00184 Roma RM, Italy",
+    creator: "u3",
+  },
+];
